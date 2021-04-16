c4f76daf1ea867421c7aceff12f57a6638087eb5
Dotexe88 <48573276+Dotexe88@users.noreply.github.com>
2021-04-16 10:24:05 -0700

    Add map to Map_view.XML and MapActivity.Java

--- 4022/app/src/main/java/com/example/a402/MapActivity.java
+++ 4022/app/src/main/java/com/example/a402/MapActivity.java
@@ -1,9 +1,12 @@
 package com.example.a402;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +20,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapActivity extends AppCompatActivity {
     private Button home;
@@ -49,6 +55,23 @@ public class MapActivity extends AppCompatActivity {
         Intent intent2 = new Intent(this, ProduceListActivity.class);
         startActivity(intent2);
     }
+    public void onMapSearch(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address>addressList = null;
 
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
 }
